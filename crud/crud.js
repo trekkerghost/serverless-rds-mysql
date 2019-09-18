e48099239f0550d083cb828c3159dd4917eed7da
Daniel Segovia <segoviadaniel+git@gmail.com>
2019-09-18 17:45:40 -0500

    Added CRUD module

---
+++ crud/crud.js
@@ -0,0 +1,43 @@
+const connection = require('../connection');
+const queryString = require('querystring');
+
+module.exports.findAll = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const sql = 'SELECT * FROM todos';
+    connection.query(sql, (error, rows) => {
+        if (error) {
+            callback({
+                statusCode: 500,
+                body: JSON.stringify(error)
+            })
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    todos: rows
+                })
+            })
+        }
+    })
+};
+
+
+module.exports.findOne = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const sql = 'SELECT * FROM todos WHERE id = ?';
+    connection.query(sql, [event.pathParameters.todo], (error, row) => {
+        if (error) {
+            callback({
+                statusCode: 500,
+                body: JSON.stringify(error)
+            })
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    todo: row
+                })
+            })
+        }
+    })
+};
